9586143f6fe51a71d50387745b30e48b7c4a401b
erlangga.raka <norang178@gmail.com>
2020-12-31 12:21:43 +0700

    reupload tgl 31 Tugas 9

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class TestController extends Controller
+{
+    public function searchData(Request $request){
+        $client = new Client();
+        $query = $request->keyword;
+        $req = $client->get('https://newsapi.org/v2/topheadlines?country=id&apiKey=YOURAPIKEY&q='.$query);
+        $response = $req->getBody();
+        $result = json_decode($response);
+        return view('home',['artikel'=>$result->articles]);
+       }
+       public function getData(){
+        $client = new Client();
+        $request = $client->get('https://newsapi.org/v2/top-headlines?country=id&apiKey=2c5ecacb7826476ea3d8886288deded2');
+        $response = $request->getBody();
+        $result = json_decode($response);
+        return view('home',['artikel'=>$result->articles]);
+        }
+}
